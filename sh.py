a51affbf65195be431702f06427e5fe9c65e17c7
张伟昊 <13315761854@163.com>
2020-02-15 10:32:18 +0800

    init repository

---
+++ sh.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*-coding:utf-8-*-
+
+
+import time
+
+idx = 0
+while idx < 5:
+    now_time = time.time()
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(now_time)))
+    time.sleep(1)
+    idx += 1
---
+++ t1.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+pwd
+ls
+DATE=$(date)
+echo $DATE
+pwd
